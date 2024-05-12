576e20f92a84458c4c6f9aacdfd4c71ddf87e10c
viacheslavkosh <koshelevskiy.v@gmail.com>
2024-05-12 23:16:13 +0300

    in progress

--- create_db.py
+++ create_db.py
@@ -1,35 +1,19 @@
 import sqlite3
 
-def create_tables():
+def create_db():
     try:
+    # читаємо файл зі скриптом для створення БД
+        with open('users.sql', 'r') as f:
+            sql = f.read()
+        
+    # створюємо з'єднання з БД (якщо файлу з БД немає, він буде створений)
         with sqlite3.connect('task_management.db') as con:
             cur = con.cursor()
-            # Таблиця користувачів
-            cur.execute('''CREATE TABLE IF NOT EXISTS users (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            fullname VARCHAR(100) NOT NULL,
-                            email VARCHAR(100) UNIQUE NOT NULL
-                        )''')
-
-            # Таблиця статусів завдань
-            cur.execute('''CREATE TABLE IF NOT EXISTS status (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            name VARCHAR(50) UNIQUE NOT NULL
-                        )''')
-
-            # Таблиця завдань
-            cur.execute('''CREATE TABLE IF NOT EXISTS tasks (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            title VARCHAR(100) NOT NULL,
-                            description TEXT,
-                            status_id INTEGER,
-                            user_id INTEGER,
-                            FOREIGN KEY (status_id) REFERENCES status(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
-                        )''')
+    # виконуємо скрипт із файлу, який створить таблиці в БД        
+            cur.executescript(sql)
 
     except sqlite3.Error as e:
         print("Error:", e)
 
 if __name__ == "__main__":
-    create_tables()
+    create_db()
